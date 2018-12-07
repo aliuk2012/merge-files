d1bc4223123448f21c632f751e949f36958061ab
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2018-12-07 08:50:51 +0000

    introduces updating of app/assets/sass/application.scss

--- kit/update-script.js
+++ kit/update-script.js
@@ -1,15 +1,17 @@
 const merge = require('merge')
-const originalFile = require('../app/config.js')
-const updatedFile = require('./config/config.js')
-const mergedJSON = merge(updatedFile, originalFile )
-const configGuidance = require('./config/config-help-text.js')
 const chalk = require('chalk')
 const emoji = require('node-emoji')
 let fs = require('fs')
-const oldConfig = fs.readFileSync('./app/config.js')
-let output = ''
 
 function mergeConfig(){
+  const originalFile = require('../app/config.js')
+  const updatedFile = require('./config/config.js')
+  const mergedJSON = merge(updatedFile, originalFile )
+  const configGuidance = require('./config/config-help-text.js')
+  const oldConfig = fs.readFileSync('./app/config.js')
+  let output = ''
+
+
   //For each object in merged JSON look up any guidance
   for(let key in mergedJSON){
     if (typeof configGuidance[key] !== 'undefined' ){
@@ -33,4 +35,21 @@ function mergeConfig(){
 }
 
 
+function mergeApplicationSCSS() {
+  const originalFile = fs.readFileSync(__dirname + '/../app/assets/sass/application.scss', 'utf8')
+  const updatedFile = fs.readFileSync('./kit/assets/sass/application.scss', 'utf8')
+  let newOutput = originalFile.substr(originalFile.match(/\/\/ Add extra styles here/).index + 96)
+
+  // Write out contents
+  fs.writeFile(__dirname + '/../app/assets/sass/application.scss', `${updatedFile.trim()}${newOutput}`, function(err){
+    if(err){
+      fs.writeFileSync(__dirname + '/../app/assets/sass/application.scss', originalFile)
+      throw err
+    }
+    console.log(chalk.green(emoji.emojify(':floppy_disk:   App SCSS file successfully updated  :floppy_disk:')))
+  })
+
+}
+
 mergeConfig();
+mergeApplicationSCSS();
